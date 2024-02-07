1fcc9855f99d9fc69128658039dc7e5eff7532af
Gwakisa Mwangomo <87722087+gmwangomo@users.noreply.github.com>
2024-02-08 00:12:59 +0300

    Update Details much

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -37,22 +37,22 @@ const projectList = [
     url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Web Development ",
     description:
       "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+    url: "https://github.com/gmwangomo",
   },
   {
     title: "My Resume Site",
     description:
       "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+    url: "https://github.com/gmwangomo",
   },
   {
     title: "GitHub Codespaces and github.dev",
     description:
       "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+    url: "#",
   },
 ];
 
